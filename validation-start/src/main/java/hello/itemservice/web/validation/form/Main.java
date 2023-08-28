4c3179ab827bfc2362df82e3d04641a2e5272fae
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-08-28 20:19:15 +0900

    유연한 설계

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -3,64 +3,39 @@ package hello.itemservice.web.validation.form;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static int n, count;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static int arr[][];
+
+    static int zero;
+    static int one;
+    static int zero_plus_one;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = s.charAt(j) - '0';
-            }
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            sb.append(zero).append(' ').append(one).append('\n');
         }
 
-        int area = 0;
+        System.out.println(sb);
+    }
 
-        ArrayList<Integer> list = new ArrayList<>();
+    static void fibonacci(int n) {
+        //초기값 세팅
+        zero = 1;
+        one = 0;
+        zero_plus_one = 1;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                count = 0;
-                if (arr[i][j] == 1) {
-                    bfs(i, j);
-                    list.add(count);
-                    area++;
-                }
-            }
-        }
-
-        Collections.sort(list);
-
-        System.out.println(area);
-        for (Integer i : list) {
-            System.out.println(i);
+            zero = one;
+            one = zero_plus_one;
+            zero_plus_one = zero + one;
         }
     }
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
-        arr[x][y] = 0;
-
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-            count++;
-            for (int i = 0; i < 4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] == 1) {
-                    q.add(new int[]{nx, ny});
-                    arr[nx][ny] = 0;
-                }
-            }
-        }
-    }
 }
