c4790ca590c1c394c10854835cf6c12b02b32a29
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-09 15:23:04 +0900

    피보나치 Bottom up 복습

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -7,35 +7,35 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static Integer[][] dp = new Integer[41][2];
+    static int zero;
+    static int one;
+    static int zero_plus_one;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-
         int t = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             fibonacci(n);
-            sb.append(dp[n][0] + " " + dp[n][1]).append('\n');
+            sb.append(zero + " " + one).append('\n');
         }
 
         System.out.println(sb);
-
     }
 
-    static Integer[] fibonacci(int n) {
-        if (dp[n][0] == null || dp[n][1] == null) {
-            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
-            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
+    static void fibonacci(int n) {
+        zero = 1;
+        one = 0;
+        zero_plus_one = 1;
+
+        for (int i = 0; i < n; i++) {
+            zero = one;
+            one = zero_plus_one;
+            zero_plus_one = zero + one;
         }
 
-        return dp[n];
     }
+
 }
