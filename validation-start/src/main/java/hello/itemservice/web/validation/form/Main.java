ace44d575aa361148a9cd4a19248b82fe483ecaf
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-09 14:58:31 +0900

    피보나치 Top down 복습

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -3,45 +3,39 @@ package hello.itemservice.web.validation.form;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Long[][] dp;
-    static int n;
-    final static long mod = 1000000000;
+
+    static Integer[][] dp = new Integer[41][2];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        dp = new Long[n + 1][10];
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
 
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1L;
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            sb.append(dp[n][0] + " " + dp[n][1]).append('\n');
         }
 
-        long result = 0;
+        System.out.println(sb);
 
-        for (int i = 1; i <= 9; i++) {
-            result += recur(n, i);
-        }
-        System.out.println(result % mod);
     }
 
-    static long recur(int digit, int val) {//digit는 자릿수, val은 자릿값
-        if (digit == 1) {
-            return dp[digit][val];
+    static Integer[] fibonacci(int n) {
+        if (dp[n][0] == null || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
         }
 
-        if (dp[digit][val] == null) {
-            if (val == 0) {
-                dp[digit][val] = recur(digit - 1, 1); //val이 0인 경우 이전자리는 1밖에 못옴
-            } else if (val == 9) {
-                dp[digit][val] = recur(digit - 1, 8); //val이 9인 경우 이전자리는 8밖에 없음
-            } else {
-                dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
-            }
-        }
-        return dp[digit][val] % mod;
+        return dp[n];
     }
-
-
 }
