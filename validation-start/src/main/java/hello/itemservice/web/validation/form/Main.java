038f4783920c4c3c23c6b7c9875d1413febb1f58
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-10 10:37:02 +0900

    파도반 수열 Bottom up

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -3,34 +3,33 @@ package hello.itemservice.web.validation.form;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-
-    static Long[] dp = new Long[101];
+    static long[] dp = new long[101];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        dp[0] = 0L;
-        dp[1] = 1L;
-        dp[2] = 1L;
-        dp[3] = 1L;
-
         int t = Integer.parseInt(br.readLine());
+
+        padovan();
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(padovan(n)).append('\n');
+            sb.append(dp[n]).append('\n');
         }
 
         System.out.println(sb);
     }
 
-    static long padovan(int n) {
-        if (dp[n] == null) {
-            dp[n] = padovan(n - 2) + padovan(n - 3);
+    static void padovan() {
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i < 101; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
         }
-        return dp[n];
+
     }
 }
