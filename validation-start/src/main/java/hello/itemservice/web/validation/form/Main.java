7b42ee1ceeb4768d0db25384aeaa86e1ed035ebc
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-10 10:24:20 +0900

    파도반 수열 Top down

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -7,35 +7,30 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int zero;
-    static int one;
-    static int zero_plus_one;
+    static Long[] dp = new Long[101];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
+        dp[0] = 0L;
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 1L;
+
+        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            fibonacci(n);
-            sb.append(zero + " " + one).append('\n');
+            sb.append(padovan(n)).append('\n');
         }
 
         System.out.println(sb);
     }
 
-    static void fibonacci(int n) {
-        zero = 1;
-        one = 0;
-        zero_plus_one = 1;
-
-        for (int i = 0; i < n; i++) {
-            zero = one;
-            one = zero_plus_one;
-            zero_plus_one = zero + one;
+    static long padovan(int n) {
+        if (dp[n] == null) {
+            dp[n] = padovan(n - 2) + padovan(n - 3);
         }
-
+        return dp[n];
     }
-
 }
