aa4772283aa5b9d0fc1bb0871922b70f32742093
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-11 10:01:21 +0900

    2xn 타일링 Top down

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -5,31 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static long[] dp = new long[101];
 
+    static Long[] dp = new Long[1001];
+    //1 2 3
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        padovan();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            sb.append(dp[n]).append('\n');
-        }
-
-        System.out.println(sb);
-    }
-
-    static void padovan() {
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 1;
-
-        for (int i = 4; i < 101; i++) {
-            dp[i] = dp[i - 2] + dp[i - 3];
-        }
+        dp[1] = 1L;
+        dp[2] = 2L;
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(logic(n));
 
     }
+
+    static long logic(int n) {
+        if (dp[n] == null) {
+            dp[n] = logic(n - 2) + logic(n-1);
+        }
+        return dp[n] % 10007;
+    }
+
+
 }
