4672d3f950b7e376b27465f1e008658f30eddad8
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-13 19:17:21 +0900

    1, 2, 3 더하기 Top down

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -5,19 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    static Integer[] dp = new Integer[12];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        long[] dp = new long[1001];
+        int t = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 3;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(logic(n)).append('\n');
         }
 
-        System.out.println(dp[n]);
+        System.out.println(sb);
+    }
+
+    public static int logic(int n) {
+        if (dp[n] == null) {
+            dp[n] = logic(n - 3) + logic(n - 2) + logic(n - 1);
+        }
+
+        return dp[n];
     }
 
 }
