415e3fb6cd08116c775dc0f7119adbde875d563f
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-14 20:06:06 +0900

    1로 만들기 Bottom up

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -6,30 +6,24 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        dp = new Integer[n + 1];
-        dp[0] = dp[1] = 0;
 
-        System.out.println(logic(n));
-    }
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
 
-    static int logic(int n) {
-        if (dp[n] == null) {
-            if (n % 6 == 0) {
-                dp[n] = Math.min(logic(n - 1), Math.min(logic(n / 3), logic(n / 2))) + 1;
-            } else if (n % 3 == 0) {
-                dp[n] = Math.min(logic(n / 3), logic(n - 1)) + 1;
-            } else if (n % 2 == 0) {
-                dp[n] = Math.min(logic(n / 2), logic(n - 1)) + 1;
-            } else {
-                dp[n] = logic(n - 1) + 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+            }
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
             }
         }
-        return dp[n];
+        System.out.println(dp[n]);
     }
+
 }
