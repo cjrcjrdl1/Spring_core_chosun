5122f0b84626fbca34c8850419b6734685688f6b
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-16 22:39:59 +0900

    가장 긴 증가하는 부분 수열 Top down

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -3,22 +3,50 @@ package hello.itemservice.web.validation.form;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] seq;
+    static Integer[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        long[] dp = new long[91];
-        dp[0] = 0;
-        dp[1] = 1;
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 2] + dp[i - 1];
+        seq = new int[n];
+        dp = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(dp[n]);
+        for (int i = 0; i < n; i++) {
+            LIS(i);
+        }
+
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
 
     }
+
+    static int LIS(int n) {
+        if (dp[n] == null) {
+            dp[n] = 1;
+
+            for (int i = n-1; i >= 0; i--) {
+                if (seq[i] < seq[n]) {
+                    dp[n] = Math.max(dp[n], LIS(i) + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
 
 }
