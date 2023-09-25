0af2841978fbe67403b8452d9b201f4671af5b54
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-09-25 10:03:50 +0900

    진법 변환

--- validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/form/Main.java
@@ -3,41 +3,33 @@ package hello.itemservice.web.validation.form;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] dp;
-    static final int mod = 10007;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dp = new int[1001][10];
+        String n = st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
 
-        int result = 0;
+        int tmp = 1;
+        int sum = 0;
 
-        for (int i = 0; i < 10; i++) {
-            result += logic(n, i);
-            result %= mod;
+        for (int i = n.length() - 1; i >= 0; i--) {
+            char c = n.charAt(i);
+
+            if ('A' <= c && c <= 'Z') {
+                sum += (c - 'A' + 10) * tmp;
+            } else {
+                sum += (c - '0') * tmp;
+            }
+
+            tmp *= b;
         }
 
-        System.out.println(result);
-    }
-
-    public static int logic(int n, int i) {
-        if (n == 1) {
-            return dp[n][i] = 1;
-        }
-
-        if (dp[n][i] > 0) {
-            return dp[n][i];
-        }
-
-        for (int k = 0; k <= i; k++) {
-            dp[n][i] += logic(n - 1, k);
-            dp[n][i] %= mod;
-        }
-        return dp[n][i];
+        System.out.println(sum);
     }
 
 }
